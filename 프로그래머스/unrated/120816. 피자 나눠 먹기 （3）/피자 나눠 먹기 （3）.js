b3edaef184ff03d0ce98d893c3576b9edb6ca932
PachyuChepe <145348244+PachyuChepe@users.noreply.github.com>
2023-10-23 09:10:41 +0900

    [level unrated] Title: 피자 나눠 먹기 (3), Time: 2.27 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -0,0 +1,9 @@
+function solution(slice, n) {
+    // console.log("뭐임", slice)
+    // console.log("뭐임?", n)
+    // n => 졷간
+    // slice => 휫짜
+    console.log(Math.ceil(n/slice))
+    var answer = Math.ceil(n/slice)
+    return answer;
+}
