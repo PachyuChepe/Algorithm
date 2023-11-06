523c1a7bb8d692cb5b53c3fc52306559481be520
PachyuChepe <145348244+PachyuChepe@users.noreply.github.com>
2023-11-06 19:55:56 +0900

    [level 1] Title: 약수의 합, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    // var answer = 0;
+    // return answer;
+    
+    let sum = 0
+    
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            sum += i
+        }
+    }
+    return sum
+}
