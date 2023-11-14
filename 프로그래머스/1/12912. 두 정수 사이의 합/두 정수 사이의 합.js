13295063cd476904e8f7b0279738572385eefa43
PachyuChepe <145348244+PachyuChepe@users.noreply.github.com>
2023-11-14 09:32:13 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,4 +1,5 @@
 function solution(a, b) {
+    // 가우스의 합 공식
     var start = Math.min(a, b); // a와 b 중 작은 값
     var end = Math.max(a, b); // a와 b 중 큰 값
     return (end - start + 1) * (start + end) / 2;
