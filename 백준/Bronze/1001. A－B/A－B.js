cd1fad099750ccc75519120dd78d55ba29a97f0e
PachyuChepe <145348244+PachyuChepe@users.noreply.github.com>
2024-05-17 20:08:19 +0900

    [Bronze V] Title: A-B, Time: 104 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : path.join(__dirname, "input.txt");
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [A, B] = input[0].split(' ').map(Number); // 첫 번째 줄을 공백 기준으로 분리하여 숫자로 변환
+const sum = A - B;
+
+console.log(sum)
