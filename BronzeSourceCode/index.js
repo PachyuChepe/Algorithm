3a5eaa89953c8458c68e1f2974f96a130c17f0c4
PachyuChepe <pachyuchepe@gmail.com>
2024-05-17 22:02:24 +0900

    [Feat] 소스코드 양식 변경

--- BronzeSourceCode/index.js
+++ BronzeSourceCode/index.js
@@ -1,10 +1,26 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const filePath = process.platform === "linux" ? "/dev/stdin" : path.join(__dirname, "input.txt");
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const filePath =
+  process.platform === 'linux'
+    ? '/dev/stdin'
+    : path.join(__dirname, 'input.txt');
+const input = fs.readFileSync(filePath).toString().trim().split(' ');
 
-const [A, B] = input[0].split(' ').map(Number); // 첫 번째 줄을 공백 기준으로 분리하여 숫자로 변환
-const sum = A - B;
+console.log(`|\\_/|
+|q p|   /}
+( 0 )"""\\
+|"^"\`    |
+||_/=\\\\__|`);
 
-console.log(sum)
+// 입력 줄바꿈 숫자
+// const num1 = parseInt(input[0]);
+
+// 입력 일직선 숫자
+// const [A, B, C] = input.map(Number);
+
+// 문자열
+// const input = fs.readFileSync(filePath).toString().trim();
+
+// 숫자
+// const input = fs.readFileSync(filePath).toString().trim().split(' ');
